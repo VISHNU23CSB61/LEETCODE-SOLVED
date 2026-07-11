d9584bf2d13b5d52906b948acd00438eeae51b57
VISHNU S <153609450+VISHNU23CSB61@users.noreply.github.com>
2026-07-11 23:20:38 +0530

    [509] [Time Beats: 26.44%] [Memory Beats: 15.78%] - Leet2Hub

---
+++ 13-Math/509. Fibonacci Number/509. Fibonacci Number.java
@@ -0,0 +1,11 @@
+1class Solution {
+2    public int fib(int n) {
+3        if(n==1){
+4            return 1;
+5        }
+6        if(n==0){
+7            return 0;
+8        }
+9        return fib(n-1)+fib(n-2);
+10    }
+11}
