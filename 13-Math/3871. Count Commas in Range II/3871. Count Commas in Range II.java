e1881c95dbff6e6e803a8985a6bbf996ac780a24
VISHNU S <153609450+VISHNU23CSB61@users.noreply.github.com>
2026-09-09 09:33:27 +0530

    [3871] [Time Beats: N/A] [Memory Beats: N/A] - Leet2Hub

---
+++ 13-Math/3871. Count Commas in Range II/3871. Count Commas in Range II.java
@@ -0,0 +1,1 @@
+class Solution {    public long countCommas(long n) {        long ans=0;        for(long base=1000;base<=n;base*=1000){            ans+=(n-base+1);            if(base>Long.MAX_VALUE/1000)break;        }                        return ans;    }}
