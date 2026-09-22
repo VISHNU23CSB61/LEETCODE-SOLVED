7b777403a7c63b102b432a24ca5827ad344f4f73
VISHNU S <153609450+VISHNU23CSB61@users.noreply.github.com>
2026-09-22 09:36:12 +0530

    [48] [Time Beats: 100.00%] [Memory Beats: 69.92%] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/48. Rotate Image/48. Rotate Image.py
@@ -0,0 +1,11 @@
+1class Solution:
+2    def rotate(self, matrix: list[list[int]]) -> None:
+3        
+4        Do not return anything, modify matrix in-place instead.
+5        
+6        n=len(matrix)
+7        for i in range(n):
+8            for j in range(i+1,n):
+9                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+10            matrix[i].reverse()
+11        return matrix
